545d36f695c23a801e3b8e18f99bbcf5179c6e30
Lauren Boyd <lboyd@esri.com>
2022-11-14 17:59:04 -0800

    Add symbology and icon for webpage

--- src/main.js
+++ src/main.js
@@ -1,154 +1,212 @@
-require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/widgets/Popup"], (Map, MapView, FeatureLayer, Popup) => {
-    const map = new Map({
-      basemap: "topo-vector"
-    });
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "esri/core/reactiveUtils",
+  "esri/widgets/Legend"
+], (Map, MapView, FeatureLayer, reactiveUtils, Legend) => {
+  const map = new Map({
+    basemap: "satellite",
+  });
 
-    const view = new MapView({
-      container: "viewDiv",
-      map: map,
-      popup: new Popup({
-        defaultPopupTemplateEnabled: true
-      })
-    });
-
-    // World countries layer.
-    const countriesLayer = new FeatureLayer({
-      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/FIFA_Test/FeatureServer/1",
-      popupTemplate: {
-        title: "{COUNTRY}",
-        outFields: ['*'],
-        content:[
-          {
-            type: "relationship",
-            relationshipId: 0,
-            title: "Stadiums in {Country}",
-            description: "Largest stadiums played in for the FIFA World Cup from 1930-2018 in {COUNTRY}.",
-            displayCount: 2,
-            orderByFields: [
-              {
-                field: "Capacity",
-                order: "desc"
-              }
-            ]
-          },
-          {
-            type: "relationship",
-            relationshipId: 2,
-            title: "Victories",
-            description: "The number of times {COUNTRY} won first place in the FIFA World Cup from 1930-2018.",
-            displayCount: 2,
-            orderByFields: [
-              {
-                field: "YEAR",
-                order: "desc"
-              }
-            ]
-          },
-          {
-            type: "relationship",
-            relationshipId: 3,
-            title: "Second Place",
-            description: "The number of times {COUNTRY} got second place in the FIFA World Cup from 1930-2018.",
-            displayCount: 2,
-            orderByFields: [
-              {
-                field: "YEAR",
-                order: "desc"
-              }
-            ]
-          },
-          {
-            type: "relationship",
-            relationshipId: 1,
-            title: "Third Place",
-            description: "The number of times {COUNTRY} got third place in the FIFA World Cup from 1930-2018.",
-            displayCount: 2,
-            orderByFields: [
-              {
-                field: "YEAR",
-                order: "desc"
-              }
-            ]
-          }
-        ]
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    popup: {
+      dockEnabled: true,
+      dockOptions: {
+        breakpoint: false,
+        position: "top-right"
       }
-    });
+    },
+  });
 
-    // Largest stadium locations for every FIFA World Cup.
-    const stadiumLayer = new FeatureLayer({
-      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/FIFA_Test/FeatureServer/0",
-      popupTemplate: {
-        title: "{StadiumName}",
-        outFields: ['*'],
-        fieldInfos: [
+  // World countries layer.
+  const countriesLayer = new FeatureLayer({
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/FIFA_World_Cup/FeatureServer/1",
+    title: "World Countries Generalized",
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-fill",
+        color: null,
+        outline: {
+          color: "#FEC310",
+          width: 0.5
+        }
+      },
+    },
+    popupTemplate: {
+      title: "{COUNTRY}",
+      outFields: ["*"],
+      content: [
+        {
+          type: "relationship",
+          relationshipId: 0,
+          title: "Stadiums in {Country}",
+          description:
+            "Largest stadiums played in for the FIFA World Cup from 1930-2018 in {COUNTRY}.",
+          displayCount: 2,
+          orderByFields: [
+            {
+              field: "Capacity",
+              order: "desc",
+            },
+          ],
+        },
+        {
+          type: "relationship",
+          relationshipId: 2,
+          title: "Victories",
+          description:
+            "The number of times {COUNTRY} won first place in the FIFA World Cup from 1930-2018.",
+          displayCount: 2,
+          orderByFields: [
+            {
+              field: "YEAR",
+              order: "desc",
+            },
+          ],
+        },
+        {
+          type: "relationship",
+          relationshipId: 3,
+          title: "Second Place",
+          description:
+            "The number of times {COUNTRY} got second place in the FIFA World Cup from 1930-2018.",
+          displayCount: 2,
+          orderByFields: [
+            {
+              field: "YEAR",
+              order: "desc",
+            },
+          ],
+        },
+        {
+          type: "relationship",
+          relationshipId: 1,
+          title: "Third Place",
+          description:
+            "The number of times {COUNTRY} got third place in the FIFA World Cup from 1930-2018.",
+          displayCount: 2,
+          orderByFields: [
+            {
+              field: "YEAR",
+              order: "desc",
+            },
+          ],
+        },
+      ],
+    },
+  });
+
+  // Largest stadium locations for every FIFA World Cup.
+  const stadiumLayer = new FeatureLayer({
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/FIFA_World_Cup/FeatureServer/0",
+    title: "FIFA World Cup Largest Stadiums",
+    popupTemplate: {
+      title: "{StadiumName} ({Year})",
+      outFields: ["*"],
+      fieldInfos: [
         {
           fieldName: "City",
         },
         {
-          fieldName: "Country"
+          fieldName: "Country",
         },
         {
           fieldName: "Year",
-          label: "World Cup year used"
+          label: "World Cup year used",
         },
         {
           fieldName: "Capacity",
           format: {
-            digitSeparator: true
-          }
+            digitSeparator: true,
+          },
         },
-        ],
-        content:[
-          {
-            type: "fields"
-          }
-        ]
-      }
-    });
+      ],
+      content: [
+        {
+          type: "fields",
+        },
+      ],
+    },
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "picture-marker",
+        url: "https://lboyd93.github.io/fifa-world-cup/resources/soccerball.svg",
+        width: "32px",
+        height: "32px"
+      },
+    },
+  });
 
-    // Non-spatial table containing FIFA World Cup standings and stats.
-    const winnersTable = new FeatureLayer({
-      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/FIFA_Test/FeatureServer/2",
-      popupTemplate: {
-        title: "FIFA World Cup {Year} Standings in {Host}",
-        outFields: ['*'],
-        fieldInfos: [
+  // Non-spatial table containing FIFA World Cup standings and stats.
+  const winnersTable = new FeatureLayer({
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/FIFA_World_Cup/FeatureServer/2",
+    title: "Country Statistics",
+    popupTemplate: {
+      title: "FIFA World Cup {Year} Standings in {Host}",
+      outFields: ["*"],
+      fieldInfos: [
         {
           fieldName: "First",
         },
         {
-          fieldName: "Second"
+          fieldName: "Second",
         },
         {
-          fieldName: "Third"
+          fieldName: "Third",
         },
         {
-          fieldName: "Fourth"
+          fieldName: "Fourth",
         },
         {
           fieldName: "TopScorer",
-          label: "Top scorers and number of goals"
+          label: "Top scorers and number of goals",
         },
         {
           fieldName: "BestPlayerAward",
-          label: "Best player award"
+          label: "Best player award",
         },
-        ],
-        content:[
-          {
-            type: "fields"
-          }
-        ]
-      }
-    });
-    // Load the non-spatial table and add it to the map's tables.
-    winnersTable.load().then(()=>{
-      map.tables.add(winnersTable);
-    });
-
-    map.layers.addMany([countriesLayer, stadiumLayer]);
-
-    stadiumLayer.when(()=>{
-      view.extent = stadiumLayer.fullExtent;
-    })
+      ],
+      content: [
+        {
+          type: "fields",
+        },
+      ],
+    },
   });
+  // Load the non-spatial table and add it to the map's tables.
+  winnersTable.load().then(() => {
+    map.tables.add(winnersTable);
+  });
+
+  map.layers.addMany([countriesLayer, stadiumLayer]);
+
+  view.when(() => {
+    view.extent = stadiumLayer.fullExtent;
+    view.whenLayerView(countriesLayer).then((countriesLayerView) => {
+      // Use reactiveUtils to watch when the layerview is done updating once.
+      reactiveUtils
+        .whenOnce(() => !countriesLayerView?.updating)
+        .then(() => {
+          // Create a query from the layerview.
+          let query = countriesLayerView.createQuery();
+          query.objectIds = [32]
+          query.outFields = countriesLayerView.availableFields;
+          // Query for the Sonoma-Lake-Napa Unit and open it's popup.
+          countriesLayerView.queryFeatures(query).then((results) => {
+            view.popup.open({
+              features: results.features[0],
+              location: results.features[0].geometry.centroid,
+              fetchFeatures: true
+            });
+          });
+        });
+    });
+  });
+
+  view.ui.add(new Legend({view:view}), "bottom-left");
+});
